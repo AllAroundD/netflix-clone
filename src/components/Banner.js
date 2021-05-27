0b3122a39d404428165bcebbbef164321dec9d67
AllAroundD <AllAroundDWeb@gmail.com>
2021-05-27 18:11:03 -0400

    feat: Banner.js - converted css to styled components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,64 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import axios from "../axios";
 import requests from "../requests";
-import "./Banner.css";
+
+const BannerContainer = styled.header`
+  color: white;
+  object-fit: contain;
+  height: 448px;
+`;
+
+const BannerContentsDiv = styled.div`
+  margin-left: 30px;
+  padding-top: 140px;
+  height: 190px;
+`;
+
+const BannerTitle = styled.h1`
+  font-size: 3rem;
+  font-weight: 800;
+  padding-bottom: 0.3rem;
+`;
+
+const BannerButton = styled.button`
+  cursor: pointer;
+  color: #fff;
+  outline: none;
+  font-weight: 700;
+  border-radius: 0.2vw;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  margin-right: 1rem;
+  padding-top: 0.5rem;
+  background-color: rgba(51, 51, 51, 0.5);
+  padding-bottom: 0.5rem;
+
+  &:hover {
+    color: #000;
+    background-color: #e6e6e6;
+    transition: all 0.2s;
+  }
+`;
+
+const BannerDescription = styled.h1`
+  width: 45rem;
+  line-height: 1.3;
+  padding-top: 1rem;
+  font-size: 0.8rem;
+  max-width: 360px;
+  height: 80px;
+`;
+
+const BannerFadeBottom = styled.div`
+  height: 7.4rem;
+  background-image: linear-gradient(
+    180deg,
+    transparent,
+    rgba(37, 37, 37, 0.61),
+    #111
+  );
+`;
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -26,29 +83,28 @@ const Banner = () => {
   };
 
   return (
-    <header
-      className="banner"
+    <BannerContainer
       style={{
         backgroundSize: "cover",
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner__contents">
-        <h1 className="banner__title">
+      <BannerContentsDiv>
+        <BannerTitle>
           {movie.title || movie.name || movie.original_name}
-        </h1>
+        </BannerTitle>
 
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+          <BannerButton>Play</BannerButton>
+          <BannerButton>My List</BannerButton>
         </div>
 
-        <h1 className="banner__description">{truncate(movie.overview, 150)}</h1>
-      </div>
+        <BannerDescription>{truncate(movie.overview, 150)}</BannerDescription>
+      </BannerContentsDiv>
 
-      <div className="banner--fadeBottom" />
-    </header>
+      <BannerFadeBottom />
+    </BannerContainer>
   );
 };
 
